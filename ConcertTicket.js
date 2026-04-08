907342e6dc1367f2c81b6f6dfc7226d29a4ac5ce
thanacohtsanprasert <t.sprst.work@gmail.com>
2026-04-08 14:11:00 +0700

    done ConcertTicket

---
+++ ConcertTicket.js
@@ -0,0 +1,12 @@
+class ConcertTicket {
+  constructor(zone, price) {
+    this.zone = zone;
+    this.price = price;
+  }
+
+  generalFoodProvide(){
+  console.log(`${this.name} Get Free Water and Crips`);
+  }
+}
+
+module.exports = ConcertTicket;
