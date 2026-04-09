857371981779fd1b1949df14034d0c244a8a27b6
poomichopoom-p <Poomicho.p@gmail.com>
2026-04-09 08:34:56 +0700

    Add_Verify

--- VipTicket.js
+++ VipTicket.js
@@ -1,23 +1,28 @@
 class VipTicket extends ConcertTicket {
+  constructor(zone, price, benefits) {
+    super(zone, price);
+    this.benefits = benefits;
+  }
 
-    constructor(zone,price,benefits){
-        super(zone,price);
-        this.benefits = benefits;
-        }
-        
-    
-    pize (price){
-        if (price >= 100000){
-            console.log(`${this.price} Free Beer 1 tower.`)
-           }else if (this.price <= 99999){
-               console.log(`${this.price} wallpaper`)}
-            
-               return console.log(`${this.price} next time.`)
-            
-            
-        }
-                    
+  pize(price) {
+    if (price >= 100000) {
+      console.log(`${this.price} Free Beer 1 tower.`);
+    } else if (this.price <= 99999) {
+      console.log(`${this.price} wallpaper`);
+    }
+
+    return console.log(`${this.price} next time.`);
+  }
+  Verify(zone) {
+    if (zone === VipTicket) {
+      console.log(`${this.zone} is VIP`);
+    } else {
+      console.log(`${this.zone} You Not VIP`);
+    }
+  }
 }
+
+Module.exports = VipTicket;
 
 // const 68 = new68(Vip,120000,kiss)
 
